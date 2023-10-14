393b6ce44e7e729863bd97987af20695989f2c68
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-14 18:25:19 -0400

    Add case statement for various high level stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,12 +1,41 @@
 import { useState, useEffect } from "react";
 
-const Stats = () => {
+const Stats = ({ statType, value, imageUrl }) => {
     const[topArtist, getTopArtist] = useState('');
     const[topGenre, getTopGenre] = useState('');
     const[minutesPlayed, getMinutesPlayed] = useState(0);
 
+    let content;
+    switch (statType) {
+        case "topArtist":
+        content = (
+            <>
+                <img className='h-2' src={imageUrl} alt="Top Artist" />
+                <p>Top Artist: {value}</p>
+            </>
+        );
+        break;
+        case "topGenre":
+        content = (
+            <>
+                <p>Top Genre: {value}</p>
+            </>
+        );
+        break;
+        case "minutesPlayed":
+        content = (
+            <>
+                <p>Minutes Played: {value}</p>
+            </>
+        );
+        break;
+        default:
+        content = <p>No valid statistic type provided.</p>;
+    }
+
     return (
         <>
+            {content}
         </>
     );
 }
