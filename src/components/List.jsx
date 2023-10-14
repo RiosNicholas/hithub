bf1f5c34313ac21141bc80496ef769e1ae59fa9d
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-14 14:48:06 -0400

    Create table boilerplate for API call

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,44 @@
+import { useState, useEffect } from "react";
+
 const List = () => {
+    const[topTracks, setTopTracks] = useState([]);
+
+    useEffect(() => {
+        response = fetch()
+
+        setTopTracks(response);
+    }, []);
+    
     return (
-        <ul>
-            <li>
-            </li>
-        </ul>
+        <div>
+            <input
+                type="text"
+                placeholder="Search..."
+                //   onChange={(inputString) => searchItems(inputString.target.value)}
+            />
+            
+            <table>
+                <h2>Top Tracks of the Month</h2>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Track</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {topTracks.map((track, index) => (
+                        <tr key={track.id}>
+                            <td>{index + 1}</td>
+                            <td>{track.name}</td>
+                            <td>{track.artist}</td>
+                            <td>{track.album}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
