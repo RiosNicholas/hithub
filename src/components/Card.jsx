0cdd3fe32c06ab8341af323e24e5201a62e66a49
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-14 15:00:44 -0400

    Add deconstructed props and CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,13 @@
-const Card = () => {
+const Card = ({ content, title }) => {
     return (
-        <>
-        </>
+        <div className="flex flex-col justify-center items-center bg-gray-600 bg-opacity-40 rounded-lg w-full m-1 p-5">
+            <h2 className="text-lg font-bold text-center">
+                {title}
+            </h2>
+            <div>
+                {content}
+            </div>
+        </div>
     );
 }
 
