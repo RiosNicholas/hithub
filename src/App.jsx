e895e64b23a1c2ff9d21eb5e443b805cf84e1c9e
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-14 13:58:03 -0400

    Import Spotify API Key

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 import './App.css'
 import Header from './components/Header'
 import Card from './components/Card'
@@ -8,18 +9,20 @@ import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <>
-      <aside className='bg-gray-700 bg-opacity-40 py-10 h-screen flex flex-col items-center'>
+    <div className="flex">
+      <aside className="bg-gray-700 bg-opacity-40 py-10 h-screen flex flex-col items-center">
         <Header />
         <NavBar />
       </aside>
-      {/* <main className='grid grid-cols-3'> */}
-        {/* <Card /> */}
-        {/* <Card /> */}
-        {/* <Card /> */}
-        <Card />
-      {/* </main> */}
-    </>
+      <main className="mx-10 my-4 grid grid-cols-3 gap-4">
+          <Card />
+          <Card />
+          <Card />
+          <div className="col-span-3">
+            <Card className="h-full"/>
+          </div>
+      </main>
+    </div>
   )
 }
 
