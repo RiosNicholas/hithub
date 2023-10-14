55570258332c97a41d28761f498889a8a8e197cb
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-14 18:18:18 -0400

    Create Stats component with states

---
+++ src/components/Stats.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const Stats = () => {
+    const[topArtist, getTopArtist] = useState('');
+    const[topGenre, getTopGenre] = useState('');
+    const[minutesPlayed, getMinutesPlayed] = useState(0);
+
+    return (
+        <>
+        </>
+    );
+}
+export default Stats;
